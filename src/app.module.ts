35ab97f03ccd612383a65b348fef6c0205c265b9
sandrine-proenca <sandrine.proenca@sncf.fr>
2023-03-20 12:34:27 +0100

    fix connections

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Produit } from './produits/entities/produit.entity';
 import { ProduitsModule } from './produits/produits.module';
 
 @Module({
-  imports: [ProduitsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [Produit],
+      synchronize: true,
+      logging: false
+    }),
+    ProduitsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {constructor(private datasource: DataSource) { }}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const config = new DocumentBuilder()
+    .setTitle('crudproduit')
+    .setDescription('EVALUATION')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+
+  app.useGlobalPipes(new ValidationPipe({
+
+    whitelist: true,
+    forbidNonWhitelisted: true,
+
+  }))
+  await app.listen(8000);
 }
 bootstrap();
