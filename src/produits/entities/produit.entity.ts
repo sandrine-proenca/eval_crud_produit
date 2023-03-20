ebb7516a6b97f7472710439da3bdf47044cb38b9
sandrine-proenca <sandrine.proenca@sncf.fr>
2023-03-20 10:01:52 +0100

    fix: entity

--- src/produits/entities/produit.entity.ts
+++ src/produits/entities/produit.entity.ts
@@ -1,1 +1,22 @@
-export class Produit {}
+import { ApiProperty } from "@nestjs/swagger";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity('produits')
+export class Produit {
+    @ApiProperty()
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @ApiProperty()
+    @Column()
+    name: string
+
+    @ApiProperty()
+    @Column()
+    price: number
+
+    @ApiProperty()
+    @Column()
+    quantity:string
+}
